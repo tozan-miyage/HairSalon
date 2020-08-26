ea1a11427b1404cfe0edbe4f8ae7e5b7b1e3cb94
genmiyazato <sapporogentaro@gmail.com>
2020-08-26 15:02:59 +0900

    width 320px ver.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 $(function () {
-
     $('a[href^="#"]').click(function () {
         var speed = 500;
         var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
+        //ヘッダーの高さを取得
+        var header = $('header').height();
+        //ヘッダーの高さを引く
+        var position = target.offset().top - header;
         $("html, body").animate({
             scrollTop: position
         }, speed, "swing");
         return false;
     });
-
 });

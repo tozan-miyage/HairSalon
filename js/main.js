6ab49a0181c3a6974cd0ca800a52af9a29728c0b
genmiyazato <sapporogentaro@gmail.com>
2020-08-26 17:10:01 +0900

    animate scroll add

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 $(function () {
+
     $('a[href^="#"]').click(function () {
-        var speed = 500;
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        //ヘッダーの高さを取得
-        var header = $('header').height();
-        //ヘッダーの高さを引く
-        var position = target.offset().top - header;
+        // headerの高さを指定
+        let headerHeight = 80;
+        // animateの速さを指定
+        let speed = 1000;
+        // クリックされた要素のhref属性を取得
+        let href = $(this).attr("href");
+        // href属性が#かつ空白であれば、'html',そうでなければ、変数hrefを格納
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        // ヘッダーの高さを引いたtargetの座標を取得
+        let position = target.offset().top - headerHeight;
+
+        // html,bodyをアニメーション
         $("html, body").animate({
             scrollTop: position
         }, speed, "swing");
+
+        // ハンバーガーメニューを閉じる
+        $(".collapse").collapse('hide');
         return false;
     });
+
+
+
 });
